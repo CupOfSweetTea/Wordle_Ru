e1b225795a1d8ee2ae11cc23246691b990097476
CupOfSweetTea <darkwizard9367@gmail.com>
2022-05-16 20:37:49 +0300

    Fix

--- Wordle/wordle.h
+++ Wordle/wordle.h
@@ -1,8 +1,8 @@
 #pragma once
-void startScreen(void);
-
+//================== Game ==================
 int wordleGame(void);
 
+//====== Screen (print, color, etc.) =======
+void startScreen(void);
 void set_hint_color(int hint_color);
-
 void print_on_screen(char attempts1[], char answers[], int attempts[], int N);
